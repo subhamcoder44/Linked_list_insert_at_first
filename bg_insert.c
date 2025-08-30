f9eab501c1d9e82f701f62cc35e4e06917a82ce5
Subham Roy <145918154+subhamcoder44@users.noreply.github.com>
2025-08-31 00:16:20 +0530

    Add files via upload

---
+++ bg_insert.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void insert(struct node *pre)
+{
+    while (pre != NULL)
+    {
+        printf("%d \n", pre->data);
+        pre = pre->next;
+    }
+}
+
+struct node *insertFirst(struct node *head, int data)
+{
+    struct node *pre = (struct node *)malloc(sizeof(struct node));
+    pre->next = head;
+    pre->data = data;
+    return pre;
+}
+
+int main()
+{
+    struct node *head;
+    struct node *sec;
+    struct node *third;
+    head=   (struct node *) malloc (sizeof (struct node ));
+    sec=(struct node *) malloc (sizeof (struct node ));
+    third=(struct node *) malloc (sizeof (struct node ));
+    head->data = 10;
+    head->next = sec;
+    
+    sec->data = 20;
+    sec->next = third;
+    
+    third->data = 30;
+    third->next = NULL;
+    
+    printf("Linked List elements:\n");
+    insert(head);
+
+    return 0;
+}
